8c9f332603bef0c707a82f4906dedb803b268e82
Alejandro <ftvalex@hotmail.com>
2022-04-06 13:39:36 +0200

    Commit 1
    trabajando en el header

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const {src , dest, watch} = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+
+function css(done){
+    src('src/scss/**/*.scss')//identificar el archivo Sass a compilar
+        .pipe(plumber())
+        .pipe(sass()) //compilarlo
+        .pipe(dest('build/css')) //almacenarlo
+
+    done();
+}
+
+function dev(done){
+    watch('src/scss/**/*.scss', css);
+
+    done();
+}
+
+
+exports.css=css;
+exports.dev=dev;
