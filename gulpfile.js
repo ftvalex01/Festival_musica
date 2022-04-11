949431c7140fa7bc6f05c2d2e2d905b94a32c00e
Alejandro <ftvalex@hotmail.com>
2022-04-11 14:22:58 +0200

    fin

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,12 @@ const imagemin = require ('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require ('gulp-avif');
 
+//Javascript
+const terser = require('gulp-terser-js');
+
+
+
+
 function css(done) {
     src('src/scss/**/*.scss')//identificar el archivo Sass a compilar
         .pipe(sourcemaps.init())
@@ -60,6 +66,9 @@ function versionAvif(done) {
 }
 function javascript(done){
     src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
     .pipe(dest('build/js'));
     done()
 }
