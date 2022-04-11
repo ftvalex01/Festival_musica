de932a096fb4b5dc2224d8ceddd0b567d4827b5c
Alejandro <ftvalex@hotmail.com>
2022-04-11 13:34:00 +0200

    Commit 6
    Terminada la sección de entradas

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded' ,function(){
+document.addEventListener('DOMContentLoaded', function () {
     iniciarApp();
 });
 
-function iniciarApp(){
+function iniciarApp() {
     crearGaleria();
 }
 
-function crearGaleria(){
-    const galeria=document.querySelector('.galeria-imagenes');
+function crearGaleria() {
+    const galeria = document.querySelector('.galeria-imagenes');
 
-    for(let i = 1 ; i <=12 ; i++){
+    for (let i = 1; i <= 12; i++) {
         const imagen = document.createElement('picture');
         imagen.innerHTML = `
         <source srcset="build/img/thumb/${i}.avif" type="image/avif">
@@ -17,42 +17,43 @@ function crearGaleria(){
         <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
         `;
         galeria.appendChild(imagen)
-        imagen.onclick = function(){
+        imagen.onclick = function () {
             mostrarImagen(i)
         }
-        function mostrarImagen(id){
+        function mostrarImagen(id) {
             const imagen = document.createElement('picture');
-        imagen.innerHTML = `
+            imagen.innerHTML = `
         <source srcset="build/img/grande/${id}.avif" type="image/avif">
         <source srcset="build/img/grande/${id}.webp" type="image/webp">
         <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galeria">
         `;
-        //Crea el overlay con la imagen
-        const overlay = document.createElement('DIV');
-        overlay.appendChild(imagen);
-        overlay.classList.add('overlay');
-        overlay.onclick = function (){
-            const body = document.querySelector('body');
-            body.classList.remove('fijar-body');
-            overlay.remove()
-        }
-        //boton para cerrar foto
-        const cerrarModal = document.createElement('P');
-        cerrarModal.textContent = 'X';
-        cerrarModal.onclick = function(){
-            const body = document.querySelector('body');
-            body.classList.remove('fijar-body');
-            overlay.remove()
-        }
-        
-        cerrarModal.classList.add('btn-cerrar');
 
-        overlay.appendChild(cerrarModal)
-        //añadirlo al HTML
-        const body = document.querySelector('body');
-        body.appendChild(overlay);
-        body.classList.add('fijar-body');
-    }
+            //Crea el overlay con la imagen
+            const overlay = document.createElement('DIV');
+            overlay.appendChild(imagen);
+            overlay.classList.add('overlay');
+            overlay.onclick = function () {
+                const body = document.querySelector('body');
+                body.classList.remove('fijar-body');
+                overlay.remove()
+            }
+
+            //boton para cerrar foto
+            const cerrarModal = document.createElement('P');
+            cerrarModal.textContent = 'X';
+            cerrarModal.onclick = function () {
+                const body = document.querySelector('body');
+                body.classList.remove('fijar-body');
+                overlay.remove()
+            }
+            cerrarModal.classList.add('btn-cerrar');
+            overlay.appendChild(cerrarModal);
+            
+            //añadirlo al HTML
+            const body = document.querySelector('body');
+            body.appendChild(overlay);
+            body.classList.add('fijar-body');
+        }
     }
 }
 
